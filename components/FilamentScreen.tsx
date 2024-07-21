93b9891065a7bf6733264b75c6a2f5837f4870c0
ROFI <rodrigofigueroa.name@gmail.com>
2024-07-20 21:32:13 -0400

    filament screen and model

---
+++ components/FilamentScreen.tsx
@@ -0,0 +1,26 @@
+import {
+  FilamentScene,
+  FilamentView,
+  DefaultLight,
+  Model,
+  Camera,
+} from "react-native-filament";
+import HiphopGirl from "../assets/models/hiphopgirl.glb";
+
+export function FilamentScreen() {
+  return (
+    <FilamentScene>
+      {/* 🏞️ A view to draw the 3D content to */}
+      <FilamentView style={{ flex: 1 }}>
+        {/* 💡 A light source, otherwise the scene will be black */}
+        <DefaultLight />
+
+        {/* 📦 A 3D model */}
+        <Model source={HiphopGirl} />
+
+        {/* 📹 A camera through which the scene is observed and projected onto the view */}
+        <Camera />
+      </FilamentView>
+    </FilamentScene>
+  );
+}
